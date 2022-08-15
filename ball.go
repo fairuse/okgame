1d9f63abff7ffe41cab1ecdc2f9cc51d52a1af1c
me <unset>
2022-08-15 16:18:52 +0200

    basic bouncing against edges of screen

--- ball.go
+++ ball.go
@@ -1,38 +1,67 @@
 package main
 
 import (
-	"math"
+//	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
 	"image/color"
-        "github.com/hajimehoshi/ebiten/v2"
+	"math"
 )
 
+func line(sx float64, sy float64, tx float64, ty float64, col color.RGBA) (vertices []ebiten.Vertex, indices []uint16) {
+	vertices = make([]ebiten.Vertex, 4)
+	indices = []uint16{0, 1, 3, 1, 2, 3}
+
+	cr := float32(col.R) / 0xff
+	cg := float32(col.G) / 0xff
+	cb := float32(col.B) / 0xff
+	ca := float32(col.A) / 0xff
+
+	l := math.Sqrt((ty-sy)*(ty-sy) + (tx-sx)*(tx-sx))
+	dx := float64((tx - sx) / l * 4) // line width = 4
+	dy := float64((ty - sy) / l * 4)
+
+        perx := dy
+        pery := -dx
+        // fmt.Println(l,dx,dy, perx, pery)
+
+	vertices[0] = ebiten.Vertex{DstX: float32(sx + perx), DstY: float32(sy + pery), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+	vertices[1] = ebiten.Vertex{DstX: float32(sx - perx), DstY: float32(sy - pery), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+	vertices[2] = ebiten.Vertex{DstX: float32(tx - perx), DstY: float32(ty - pery), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+	vertices[3] = ebiten.Vertex{DstX: float32(tx + perx), DstY: float32(ty + pery), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+
+	// fmt.Println(vertices)
+
+	return vertices, indices
+
+}
+
 func ball(px float64, py float64, col color.RGBA) (vertices []ebiten.Vertex, indices []uint16) {
-     const MAXSEG = 64
-     const r = 4.0
+	const MAXSEG = 64
+	const r = 10.0
 
-     vertices = make([]ebiten.Vertex,3*MAXSEG)
-     indices = make([]uint16,3*MAXSEG)
+	vertices = make([]ebiten.Vertex, 3*MAXSEG)
+	indices = make([]uint16, 3*MAXSEG)
 
-     cr := float32(col.R)/0xff
-     cg := float32(col.G)/0xff
-     cb := float32(col.B)/0xff
-     ca := float32(col.A)/0xff
+	cr := float32(col.R) / 0xff
+	cg := float32(col.G) / 0xff
+	cb := float32(col.B) / 0xff
+	ca := float32(col.A) / 0xff
 
-     var segment uint16
-     for segment=0;segment<MAXSEG;segment+=1 {
-          dx := math.Sin(float64(segment) * 2.0 * math.Pi / MAXSEG) * r
-          dy := math.Cos(float64(segment) * 2.0 * math.Pi / MAXSEG) * r
-          dxn := math.Sin(float64(segment+1) * 2.0 * math.Pi/ MAXSEG) * r
-          dyn := math.Cos(float64(segment+1) * 2.0 * math.Pi / MAXSEG) * r
+	var segment uint16
+	for segment = 0; segment < MAXSEG; segment += 1 {
+		dx := math.Sin(float64(segment)*2.0*math.Pi/MAXSEG) * r
+		dy := math.Cos(float64(segment)*2.0*math.Pi/MAXSEG) * r
+		dxn := math.Sin(float64(segment+1)*2.0*math.Pi/MAXSEG) * r
+		dyn := math.Cos(float64(segment+1)*2.0*math.Pi/MAXSEG) * r
 
-          vertices[segment*3+0] = ebiten.Vertex{DstX:float32(px),DstY:float32(py), SrcX:1, SrcY:1, ColorR:cr, ColorG:cg, ColorB:cb, ColorA: ca}
-          vertices[segment*3+1] = ebiten.Vertex{DstX:float32(px+dx),DstY:float32(py+dy), SrcX:1, SrcY:1, ColorR:cr, ColorG:cg, ColorB:cb, ColorA: ca}
-          vertices[segment*3+2] = ebiten.Vertex{DstX:float32(px+dxn),DstY:float32(py+dyn), SrcX:1, SrcY:1, ColorR:cr, ColorG:cg, ColorB:cb, ColorA: ca}
+		vertices[segment*3+0] = ebiten.Vertex{DstX: float32(px), DstY: float32(py), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+		vertices[segment*3+1] = ebiten.Vertex{DstX: float32(px + dx), DstY: float32(py + dy), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
+		vertices[segment*3+2] = ebiten.Vertex{DstX: float32(px + dxn), DstY: float32(py + dyn), SrcX: 1, SrcY: 1, ColorR: cr, ColorG: cg, ColorB: cb, ColorA: ca}
 
-          indices[segment*3+0] = segment*3+0
-          indices[segment*3+1] = segment*3+1
-          indices[segment*3+2] = segment*3+2
-     }
+		indices[segment*3+0] = segment*3 + 0
+		indices[segment*3+1] = segment*3 + 1
+		indices[segment*3+2] = segment*3 + 2
+	}
 
-     return vertices,indices
+	return vertices, indices
 }
--- main.go
+++ main.go
@@ -1,91 +1,119 @@
 package main
 
 import (
-	"log"
 	"errors"
-        "image/color"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"image"
-        "github.com/hajimehoshi/ebiten/v2"
-        "github.com/hajimehoshi/ebiten/v2/ebitenutil"
-        "github.com/hajimehoshi/ebiten/v2/inpututil"
+	"image/color"
+	"log"
 )
 
 const screenWidth = 512
 const screenHeight = 512
 
-var  emptyImage = ebiten.NewImage(3, 3)
+var emptyImage = ebiten.NewImage(3, 3)
 
 func init() {
-	emptyImage.Fill( color.White )
+	emptyImage.Fill(color.White)
 }
 
 func (g *Game) handleMovement() {
-        tx, ty := ebiten.CursorPosition()
-        if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-                g.ballx = tx
-                g.bally = ty
-        }
-        if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-                g.targetx = tx
-                g.targety = ty
-        }
-        if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
-                g.ballx = tx
-                g.bally = ty
-        }
+	tx, ty := ebiten.CursorPosition()
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		g.ballx = float64(tx)
+		g.bally = float64(ty)
+		g.positioning = true
+	}
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		g.targetx = float64(tx)
+		g.targety = float64(ty)
+	}
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		g.speedx = (g.targetx - g.ballx) / 30.0
+		g.speedy = (g.targety - g.bally) / 30.0
+		//		g.ballx = float64(tx)
+		//		g.bally = float64(ty)
+		g.positioning = false
+	}
+
+	if !g.positioning {
+		// fmt.Println(g.ballx, g.bally, g.speedx, g.speedy)
+		g.ballx += g.speedx
+		g.bally += g.speedy
+
+		if g.ballx > screenWidth && g.speedx > 0 {
+			g.speedx *= -1
+		}
+		if g.bally > screenHeight && g.speedy > 0 {
+			g.speedy *= -1
+		}
+
+		if g.ballx < 0 && g.speedx < 0 {
+			g.speedx *= -1
+		}
+		if g.bally < 0 && g.speedy < 0 {
+			g.speedy *= -1
+		}
+	}
 }
 
 type Game struct {
-        showRays bool
-        ballx, bally   int
-        targetx, targety   int
-//        objects  []object
+	showRays         bool
+	ballx, bally     float64
+	targetx, targety float64
+	speedx, speedy   float64
+	positioning      bool
+	//        objects  []object
 }
 
 func (g *Game) Update() error {
-        if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
-                return errors.New("game ended by player")
-        }
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return errors.New("game ended by player")
+	}
 
-        if inpututil.IsKeyJustPressed(ebiten.KeyR) {
-                g.showRays = !g.showRays
-        }
+	if inpututil.IsKeyJustPressed(ebiten.KeyR) {
+		g.showRays = !g.showRays
+	}
 
-        g.handleMovement()
+	g.handleMovement()
 
-        return nil
+	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-        src := emptyImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
+	src := emptyImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
 
-        ebitenutil.DebugPrintAt(screen, "Click and drag to launch ball", 0, 0)
-	ebitenutil.DebugPrintAt(screen, "Ball", g.ballx, g.bally)
-	ebitenutil.DebugPrintAt(screen, "Target", g.targetx, g.targety)
+	if g.positioning {
+		v, i := line(float64(g.ballx), float64(g.bally), float64(g.targetx), float64(g.targety), color.RGBA{0x40, 0x00, 0x80, 0xff})
+		screen.DrawTriangles(v, i, src, nil)
+		v, i = ball(float64(g.targetx), float64(g.targety), color.RGBA{0xa0, 0xa0, 0xa0, 0xff})
+		screen.DrawTriangles(v, i, src, nil)
+	}
 
-        v,i := ball(float64(g.ballx),float64(g.bally),color.RGBA{0xff,0xff,0,0xff})
-        screen.DrawTriangles(v, i, src, nil)
+	v, i := ball(float64(g.ballx), float64(g.bally), color.RGBA{0xff, 0xff, 0, 0xff})
+	screen.DrawTriangles(v, i, src, nil)
 
-        v,i = ball(float64(g.targetx),float64(g.targety),color.RGBA{0xa0,0xa0,0xa0,0xff})
-        screen.DrawTriangles(v, i, src, nil)
+	ebitenutil.DebugPrintAt(screen, "Click and drag to launch ball", 0, 0)
+	//ebitenutil.DebugPrintAt(screen, "Ball", g.ballx, g.bally)
+	//ebitenutil.DebugPrintAt(screen, "Target", g.targetx, g.targety)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-        return screenWidth, screenHeight
+	return screenWidth, screenHeight
 }
-
 
 func main() {
-        g := &Game{
-                ballx: screenWidth / 2,
-                bally: screenHeight / 2,
-        }
+	g := &Game{
+		ballx: screenWidth / 2,
+		bally: screenHeight / 2,
+	}
 
-
-        ebiten.SetWindowSize(screenWidth, screenHeight)
-        ebiten.SetWindowTitle("Ray casting and shadows (Ebiten demo)")
-        if err := ebiten.RunGame(g); err != nil {
-                log.Fatal(err)
-        }
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Ray casting and shadows (Ebiten demo)")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
 }
- 
