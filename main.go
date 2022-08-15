316307ce13c70487bd7255bdb7d27cb5c0d56f73
me <unset>
2022-08-15 14:43:37 +0200

    added ball and mouse positioning

--- main.go
+++ main.go
@@ -3,26 +3,42 @@ package main
 import (
 	"log"
 	"errors"
+        "image/color"
+	"image"
         "github.com/hajimehoshi/ebiten/v2"
         "github.com/hajimehoshi/ebiten/v2/ebitenutil"
-//        "github.com/hajimehoshi/ebiten/v2/examples/resources/images"
         "github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-const screenWidth = 256
-const screenHeight = 256
+const screenWidth = 512
+const screenHeight = 512
+
+var  emptyImage = ebiten.NewImage(3, 3)
+
+func init() {
+	emptyImage.Fill( color.White )
+}
 
 func (g *Game) handleMovement() {
         tx, ty := ebiten.CursorPosition()
-        if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) && tx != 0 && ty != 0 {
-                g.px = tx
-                g.py = ty
+        if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+                g.ballx = tx
+                g.bally = ty
+        }
+        if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+                g.targetx = tx
+                g.targety = ty
+        }
+        if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+                g.ballx = tx
+                g.bally = ty
         }
 }
 
 type Game struct {
         showRays bool
-        px, py   int
+        ballx, bally   int
+        targetx, targety   int
 //        objects  []object
 }
 
@@ -41,7 +57,17 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+        src := emptyImage.SubImage(image.Rect(1, 1, 2, 2)).(*ebiten.Image)
+
         ebitenutil.DebugPrintAt(screen, "Click and drag to launch ball", 0, 0)
+	ebitenutil.DebugPrintAt(screen, "Ball", g.ballx, g.bally)
+	ebitenutil.DebugPrintAt(screen, "Target", g.targetx, g.targety)
+
+        v,i := ball(float64(g.ballx),float64(g.bally),color.RGBA{0xff,0xff,0,0xff})
+        screen.DrawTriangles(v, i, src, nil)
+
+        v,i = ball(float64(g.targetx),float64(g.targety),color.RGBA{0xa0,0xa0,0xa0,0xff})
+        screen.DrawTriangles(v, i, src, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -51,12 +77,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
         g := &Game{
-                px: screenWidth / 2,
-                py: screenHeight / 2,
+                ballx: screenWidth / 2,
+                bally: screenHeight / 2,
         }
 
 
-        ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+        ebiten.SetWindowSize(screenWidth, screenHeight)
         ebiten.SetWindowTitle("Ray casting and shadows (Ebiten demo)")
         if err := ebiten.RunGame(g); err != nil {
                 log.Fatal(err)
